1e741c89211978a0595318fe7ec02e2c35f29d87
Marta Pintos <martapintoslopez@gmail.com>
2023-11-16 19:30:23 +0100

    BBDD

--- SELENIAS_SQL/NETCHILL.sql
+++ SELENIAS_SQL/NETCHILL.sql
@@ -13,22 +13,22 @@ anio VARCHAR(45),
 PRIMARY KEY (auto_ID));
 -- Tabla Detalles Pelis, con IMDB ,RT, Direccion, Guión, Argumento, Duración, Título
 CREATE TABLE `Detalles_Peliculas` (
-IMDB VARCHAR(4),
-RT VARCHAR(4),
+IMDB VARCHAR(25),
+RT VARCHAR(25),
 direccion VARCHAR(100),
 guion VARCHAR(100),
 argumento VARCHAR(3000),
 duracion VARCHAR(45),
-titulo VARCHAR(1000),
-PRIMARY KEY (titulo));
+titulo VARCHAR(1000)
+/*PRIMARY KEY (titulo)*/);
 -- Tabla Detalles Actores, con nombre, año de nacimiento, por qué es conocido, qué hace, Premios
 CREATE TABLE `Detalles_Actores` (
 nombre VARCHAR(100),
 nacimiento VARCHAR(45),
 conocido VARCHAR(3000),
 ocupacion VARCHAR(1000),
-premios VARCHAR(100),
-PRIMARY KEY (nombre));
+premios VARCHAR(100)
+/*PRIMARY KEY (nombre)*/);
 -- Tabla Premios, con fecha_celebracion, mejor_pelicula, mejor_director, mejor_actor, mejor_actriz
 CREATE TABLE `Premios` (
 fecha_celebracion INT,
